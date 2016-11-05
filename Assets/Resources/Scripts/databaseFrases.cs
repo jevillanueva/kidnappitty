afb4c80dbcf960adc4d9e3e1f8fa5189a6802aae
SatoshiDark <ygon01@gmail.com>
2016-11-05 13:10:21 -0400

    Minigame Button

--- Assets/Resources/Scripts/databaseFrases.cs
+++ Assets/Resources/Scripts/databaseFrases.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class databaseFrases {
 	public static string[] frases;
+	public static string[] actionQuoutes;
 	// Use this for initialization
 	public databaseFrases(){
 		frases = new string[10];
@@ -17,8 +18,18 @@ public class databaseFrases {
 		frases [8] = "9";
 		frases [9] = "10";
 
-
-
-
+		// Action Quotes
+		actionQuoutes = new string[10];
+		actionQuoutes [0] = "Esquivalos!";
+		actionQuoutes [1] = "No lo pierdas de vista!";
+		actionQuoutes [2] = "3";
+		actionQuoutes [3] = "4";
+		actionQuoutes [4] = "5";
+		actionQuoutes [5] = "6";
+		actionQuoutes [6] = "7";
+		actionQuoutes [7] = "8";
+		actionQuoutes [8] = "9";
+		actionQuoutes [9] = "10";
 	}
+
 }
--- Assets/Resources/Scripts/miniGameBasic.cs
+++ Assets/Resources/Scripts/miniGameBasic.cs
@@ -22,4 +22,8 @@ public class miniGameBasic : MonoBehaviour {
 
 	}
 
+	public void loseMinigame(){
+		miniGameStatus = false;
+	}
+
 }
