260932f78c6fbb06e890e4d1299edb404c1b72f1
Jonathan <frik_ej2v@hotmail.com>
2016-11-05 12:07:02 -0400

    ultimos cambios

---
+++ Assets/Resources/Scripts/menuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class menuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void iniciarjuego(){
+		SceneManager.LoadScene (1);
+	}
+	public void repetirTodo(){
+		Debug.Log ("asdd");
+	}
+}
