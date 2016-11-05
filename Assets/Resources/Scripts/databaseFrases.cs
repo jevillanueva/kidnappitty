38ed0835a1c588af350122ca6e86f62ff1bb79fa
Jonathan <frik_ej2v@hotmail.com>
2016-11-05 12:09:59 -0400

    asdad

--- Assets/Resources/Scripts/databaseFrases.cs
+++ Assets/Resources/Scripts/databaseFrases.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class databaseFrases : MonoBehaviour {
-
+public class databaseFrases {
+	public static string[] frases;
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public databaseFrases(){
+		frases = new string[10];
+		frases [0] = "1";
+		frases [1] = "2";
+		frases [2] = "3";
+		frases [3] = "4";
+		frases [4] = "5";
+		frases [5] = "6";
+		frases [6] = "7";
+		frases [7] = "8";
+		frases [8] = "9";
+		frases [9] = "10";
+
+
+
+
 	}
 }
