f4f6422b871bc1dc8ba718813636adf61a735b48
SatoshiDark <ygon01@gmail.com>
2016-11-05 06:26:12 -0400

    Minigame 2 Reborn

---
+++ Assets/Resources/Scripts/Mecanics/CandyDodge/Collision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : miniGameBasic {
+
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		//Debug.Log (col.tag);
+		if(col.gameObject.tag == "Enemy")
+		{
+			Destroy(col.gameObject);
+			miniGameStatus = false;
+			endMiniGame ();
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Mecanics/CandyDodge/DragBasic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragBasic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if(touch.phase == TouchPhase.Moved)
+			{
+				Debug.Log ("LEL");
+			}
+
+			if(touch.phase == TouchPhase.Ended)
+			{
+				
+			}
+		}
+	}
+}
